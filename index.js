31648889444b062dd8466af224332f6873318433
Daniel Mugabo <mugabodaniel26@gmail.com>
2024-02-14 19:57:47 +0000

    initial commit

--- index.js
+++ index.js
@@ -2,13 +2,82 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 require('dotenv').config()
+const mongoose = require('mongoose')
+const { Schema } = mongoose;
+
+mongoose.connect(process.env.DB_URL)
 
 app.use(cors())
 app.use(express.static('public'))
+app.use(express.urlencoded({ extended: true }))
+
+const UserSchema = new Schema({
+  username: String,
+});
+const User = mongoose.model("User", UserSchema);
+
+const ExerciseSchema = new Schema({
+  user_id: { type: String, required: true },
+  description: String,
+  duration: Number,
+  date: Date,
+});
+const Exercise = mongoose.model("Exercise", ExerciseSchema);
+
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
 });
 
+app.get("/api/users", async (req, res) => {
+  const users = await User.find({}).select("_id username");
+  if (!users) {
+    res.send("No users");
+  } else {
+    res.json(users);
+  }
+})
+
+app.post("/api/users", async (req, res) => {
+  console.log(req.body)
+  const userObj = new User({
+    username: req.body.username,
+  })
+  try {
+    const user = await userObj.save();
+    res.json(user)
+  } catch (error) {
+    console.log(error)
+  }
+})
+
+app.post("/api/users/:_id/exercises", async (req,res) => {
+  const id = req.params._id;
+  const { description, duration,date } = req.body;
+  try {
+    const user = await User.findById(id);
+    if (!user){
+      res.send("Could not find user!");
+    } else {
+      const exerciseObj = new Exercise({
+        user_id: user._id,
+        description,
+        duration,
+        date: date ? new Date(date) : new Date()
+      })
+      const exercise = await exerciseObj.save();
+      res.json({
+        _id: user._id,
+        username: user.username,
+        description: exercise.description,
+        duration: exercise.duration,
+        date: new Date(exercise.date).toDateString()
+      })
+    }
+  } catch (error) {
+    console.log(error);
+    res.send("There was an error saving the exercise.");
+  }
+})
 
 
 
